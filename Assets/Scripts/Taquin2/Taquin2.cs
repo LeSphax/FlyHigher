16b8b4fd8d5b094b3b0d23769468c148b54561c9
Sphax <sphax31@gmail.com>
2015-01-27 21:19:51 +0100

    test

---
+++ Assets/Scripts/Taquin2/Taquin2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Taquin2 : MonoBehaviour
+{
+
+		public int height;
+		public int width;
+		private int pixelsWidthTile;
+		private int pixelsHeightTile;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+		}
+	
+		// Update is called once per frame
+}
