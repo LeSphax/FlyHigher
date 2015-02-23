4f82ecaf96dae1adec9eb40eed55a466159f976b
DindieWestWood <drouet.dimitri@gmail.com>
2015-02-23 14:38:19 +0100

    fly 1.3

---
+++ Assets/Scripts/15 - Fly/MovingObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MovingObject : MonoBehaviour {
+
+	public float speed;
+	public LayerMask blockingLayer;
+
+	private Rigidbody2D rb2D;
+	private BoxCollider2D bc2D;
+
+	protected virtual bool Move(int xDir, int yDir, out RaycastHit2D hit){
+		bc2D = GetComponent<BoxCollider2D> ();
+		rb2D = GetComponent<Rigidbody2D> ();
+		Vector2 start = transform.position;
+		
+		Vector2 end = start + new Vector2 (xDir, yDir);
+		
+		bc2D.enabled = false;
+		
+		hit = Physics2D.Linecast (start, end, blockingLayer);
+		
+		bc2D.enabled = true;
+		
+		if (hit.transform == null) {
+			MoveAction(xDir, yDir);
+			return true;
+		}
+		
+		return false;
+	}
+
+	protected abstract void MoveAction (int xDir, int yDir);
+}
