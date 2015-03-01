4f155fd260d62da68dac6c1ed69b38cf22992063
LeSphax <sphax31@gmail.com>
2015-03-01 09:32:56 +0100

    Remake loading screen

---
+++ Assets/LoadingScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour
+{
+
+    public int timer;
+    float counter;
+    public string sceneToLoad;
+    // Use this for initialization
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > timer)
+        {
+            Application.LoadLevel(sceneToLoad);
+        }
+    }
+
+}
