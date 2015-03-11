2cdb7910022d450b478792df9f9865511d5f28db
LeSphax <sphax31@gmail.com>
2015-03-11 16:16:50 +0100

    Add facts (Modify PopUp, add images to xml and to project)

---
+++ Assets/Scripts/Global/Language/GetFact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GetFact : MonoBehaviour {
+
+    public string idFact;
+
+    void Start()
+    {
+        GetComponentInChildren<Text>().text = LanguageText.Instance.GetFact(idFact).text;
+        GetComponentsInChildren<Image>()[1].sprite = Resources.Load(LanguageText.Instance.GetFact(idFact).image, typeof(Sprite)) as Sprite;
+    }
+}
