53673bf48b7f35e5da7d287f576f89ae8b96f87e
Canivek <canivek92@gmail.com>
2015-03-14 17:42:24 +0100

    Rename folders games Kevin + Add first version new game Paint

---
+++ Assets/Scripts/17 - PaintNew/ElementToPaintBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElementToPaintBehavior : MonoBehaviour
+{
+	public GameControlPaint gameControl;
+
+	private SpriteRenderer renderer ;
+	private DotColor dotColor;
+
+	void Awake ()
+	{
+		renderer = GetComponent<SpriteRenderer> ();
+		dotColor = GetComponentInChildren<DotColor> ();
+		gameControl = GameObject.FindWithTag ("GameController").GetComponent<GameControlPaint> ();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+
+	private void SetColor (Color c)
+	{
+		renderer.color = c;
+	}
+
+
+	public void ColorChanged ()
+	{
+		Color newColor = gameControl.GetSelectedColor ();
+		SetColor (newColor);
+		dotColor.SetColorUnder (newColor);
+		gameControl.CheckGameEnded ();
+	}
+
+
+	void OnMouseDown ()
+	{
+		ColorChanged ();
+	}
+
+}
