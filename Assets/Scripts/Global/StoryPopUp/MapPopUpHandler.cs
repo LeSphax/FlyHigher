593d2f008df201799d09c6ae5e6a84334325ef07
Canivek <canivek92@gmail.com>
2015-03-16 20:18:06 +0100

    Change BatimentPopUpHandler and PopUpHandler to show them only one time by game session

--- Assets/Scripts/Global/StoryPopUp/MapPopUpHandler.cs
+++ Assets/Scripts/Global/StoryPopUp/MapPopUpHandler.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MapPopUpHandler : PopUpHandler {
-    public string idStory;
+public class MapPopUpHandler : PopUpHandler
+{
+	public string idStory;
 
-    protected override System.Collections.Generic.Queue<string> GetTexts()
-    {
-        return GetTexts(idStory);
-    }
+	protected override System.Collections.Generic.Queue<string> GetTexts ()
+	{
+		return GetTexts (idStory);
+	}
 }
