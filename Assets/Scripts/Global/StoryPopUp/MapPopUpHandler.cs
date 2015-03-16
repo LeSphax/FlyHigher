7a9f9734db2779794677a93a21dc3d08139a3e0e
Canivek <canivek92@gmail.com>
2015-03-16 21:38:02 +0100

    Change MapPopUp, only show one time + Add alternative text

--- Assets/Scripts/Global/StoryPopUp/MapPopUpHandler.cs
+++ Assets/Scripts/Global/StoryPopUp/MapPopUpHandler.cs
@@ -4,9 +4,22 @@ using System.Collections;
 public class MapPopUpHandler : PopUpHandler
 {
 	public string idStory;
+	public string idAlternativeStory;
 
 	protected override System.Collections.Generic.Queue<string> GetTexts ()
 	{
-		return GetTexts (idStory);
+		if (PopUpHandler.AlreadySeen (idStory) || PopUpHandler.AlreadySeen (idAlternativeStory)) {
+			return new System.Collections.Generic.Queue<string> ();
+		} else {
+			GameData gamedata = GameObject.FindWithTag ("GameControl").GetComponent<GameData> ();
+			if (gamedata.showAlternativeText) {
+				PopUpHandler.SetPopUpAlreadySeen (idAlternativeStory);
+				return base.GetTexts (idAlternativeStory);
+			} else {
+				gamedata.showAlternativeText = true;
+				PopUpHandler.SetPopUpAlreadySeen (idStory);
+				return base.GetTexts (idStory);
+			}
+		}
 	}
 }
