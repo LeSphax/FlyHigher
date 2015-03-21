b1f2d9120cf431cd6632d634dd7ea5506fe94d87
Canivek <canivek92@gmail.com>
2015-03-21 22:32:44 +0100

    Add level chooser to Paint Game

--- Assets/Scripts/17 - PaintNew/GameControlPaint.cs
+++ Assets/Scripts/17 - PaintNew/GameControlPaint.cs
@@ -3,32 +3,76 @@ using System.Collections;
 
 public class GameControlPaint : MonoBehaviour
 {
-	public GameObject prefabDraw;
+	
+	public GameObject prefabDrawLvl1;
+	public GameObject prefabDrawLvl2;	
+	public GameObject prefabDrawLvl3;
+
 	public GameObject prefabColorJar;
 	public Color[] ColorPossible;
 	public Vector3[] positionJars;
 
-	private Color colorSelected = new Color (1f, 1f, 1f, 1f);
+	private JarBehavior jarSelected;
+	private Color colorSelected ;
 	private DotColor[] dotsColor;
 
-	// Use this for initialization
-	void Start ()
+	public void init (int level)
 	{
-		for (int i = 0; i < positionJars.Length; i++) {
-			GameObject colorJar = Instantiate (prefabColorJar, positionJars [i], Quaternion.identity) as GameObject;
-			colorJar.GetComponentInChildren<JarBehavior> ().SetColor (ColorPossible [i]);
+		dotsColor = new DotColor[5];
+		colorSelected = new Color (1f, 1f, 1f, 1f);
+		GameObject draw;
+
+		switch (level) {
+		case 1:
+			draw = Instantiate (prefabDrawLvl1) as GameObject;
+			break;
+		case 2:
+			draw = Instantiate (prefabDrawLvl2) as GameObject;
+			break;
+		case 3:
+			draw = Instantiate (prefabDrawLvl3) as GameObject;
+			break;
+		default:
+			draw = Instantiate (prefabDrawLvl1) as GameObject;
+			break;
 		}
-			
-		GameObject draw = Instantiate (prefabDraw) as GameObject;
+
+		draw.transform.SetParent (this.transform.parent);
 		dotsColor = draw.GetComponentsInChildren<DotColor> ();
 		foreach (DotColor dc in dotsColor) {
 			dc.SetColorWanted (ColorPossible [Random.Range (0, ColorPossible.Length)]);
 		}
+
+
+		for (int i = 0; i < positionJars.Length; i++) {
+			GameObject colorJar = Instantiate (prefabColorJar, positionJars [i], Quaternion.identity) as GameObject;
+			JarBehavior jar = colorJar.GetComponentInChildren<JarBehavior> ();
+			jar.Start ();
+			jar.SetColor (ColorPossible [i]);
+			colorJar.transform.SetParent (this.transform.parent);
+			jar.SetController (this);
+			if (i == 0) {
+				SetSelectedJar (jar);
+			} else {
+				jar.SetSelected (false);
+			}
+		}
+
+		foreach (ElementToPaintBehavior e in 
+		         transform.parent.gameObject.GetComponentsInChildren<ElementToPaintBehavior>()) {
+			e.SetController (this);
+		}
+
 	}
 
-	public void SetSelectedColor (Color c)
+	public void SetSelectedJar (JarBehavior jar)
 	{
-		colorSelected = c;
+		if (jarSelected != null) {
+			jarSelected.SetSelected (false);
+		}
+		jarSelected = jar;
+		jarSelected.SetSelected (true);
+		colorSelected = jarSelected.GetColor ();
 	}
 
 	public Color GetSelectedColor ()
@@ -38,9 +82,7 @@ public class GameControlPaint : MonoBehaviour
 
 	public void CheckGameEnded ()
 	{
-		Debug.Log ("CheckGameEnded");
 		for (int i = 0; i < dotsColor.Length; i++) {
-			Debug.Log (dotsColor [i].ColorIsOk ());
 			if (!(dotsColor [i].ColorIsOk ())) {
 				return;
 			}
@@ -48,8 +90,13 @@ public class GameControlPaint : MonoBehaviour
 		EndOfGame ();
 	}
 
+	public void resetLevel ()
+	{
+		Destroy (transform.parent.gameObject);
+	}
+
 	public void EndOfGame ()
 	{
-		Debug.Log ("EndOfGame");
+		GameObject.FindGameObjectWithTag ("GamesUI").GetComponent<EndLevelGamePopUp> ().LevelEnded ();
 	}
 }
--- Assets/Scripts/17 - PaintNew/JarBehavior.cs
+++ Assets/Scripts/17 - PaintNew/JarBehavior.cs
@@ -1,31 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class JarBehavior : MonoBehaviour
 {
 	public Color myColor;
+	public Sprite spriteSelected;
+	public Sprite spriteUnSelected;
 
 	private GameControlPaint gameControl;
 
-	void Awake ()
+	private	SpriteRenderer paintRenderer ;
+	private SpriteRenderer jarRenderer;
+
+	public void Start ()
 	{
-		gameControl = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameControlPaint> ();
+		jarRenderer = GetComponentsInChildren<SpriteRenderer> () [0];
+		paintRenderer = GetComponentsInChildren<SpriteRenderer> () [1];
 	}
 
-	// Use this for initialization
-	void Start ()
+	public void SetController (GameControlPaint g)
 	{
-
+		gameControl = g;
 	}
 
 	void OnMouseDown ()
 	{
-		gameControl.SetSelectedColor (myColor);
+		if (!EventSystem.current.IsPointerOverGameObject ()) {
+			gameControl.SetSelectedJar (this);
+		}
+	}
+
+	public void SetSelected (bool b)
+	{
+		if (b) {
+			jarRenderer.sprite = spriteSelected;
+		} else {
+			jarRenderer.sprite = spriteUnSelected;
+		}
 	}
 
 	public void SetColor (Color c)
 	{
 		GetComponentsInChildren<SpriteRenderer> () [1].color = c;
 		myColor = c;
+	}
+
+	public Color GetColor ()
+	{
+		return myColor;
 	}
 }
