1d46eac8f7c2180997dc9486dd8b09f8751359a0
LeSphax <sphax31@gmail.com>
2015-04-07 18:38:39 +0200

    Add first cutScene

---
+++ Assets/Scripts/Story/CrackingEgg.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrackingEgg : MonoBehaviour {
+
+
+    public GameObject egg;
+    public Sprite crackedEgg;
+    public AudioClip crackSound;
+    public string sceneToLoad;
+    
+
+    public void Crack()
+    {
+        AudioSource.PlayClipAtPoint(crackSound, transform.position);
+        egg.GetComponent<SpriteRenderer>().sprite = crackedEgg;
+    }
+
+    public void EndAnimation()
+    {
+        Application.LoadLevel(sceneToLoad);
+    }
+
+}
