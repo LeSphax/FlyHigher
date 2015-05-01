187351b4df61d63a9a3faf3eddb4b04d1b7a057f
LeSphax <sphax31@gmail.com>
2015-05-01 10:09:39 +0200

    Add Ending cutscene

---
+++ Assets/Scripts/Global/GoToEnding.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToEnding : MonoBehaviour {
+
+    GameData gameData;
+
+    void Awake()
+    {
+        gameData = GameObject.FindWithTag("GameControler").GetComponent<GameData>();
+        if (gameData.AreGamesCompletedInBuilding("ControlTower"))
+        {
+            Application.LoadLevel("Ending");
+        }
+    }
+}
